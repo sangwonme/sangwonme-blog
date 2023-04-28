c388ac02531979f37982dda1c23c9f2207df9972
sangwonme <paulmoguri@naver.com>
2023-04-28 13:51:33 +0900

    :sparkles: Add AboutMe

--- src/components/HomePageRelated/AboutMe.tsx
+++ src/components/HomePageRelated/AboutMe.tsx
@@ -5,7 +5,19 @@ import './AboutMe.scss'
 const AboutMe = () => {
     return(
         <div className='aboutme-container'>
-            
+            <div className='aboutme-wrapper'>
+                <div className='aboutme-text'>
+                    <pre className='aboutme-name'>Sangwon<br/>Park</pre>
+                    <p className='aboutme-description'>
+                        Hi. I’m Sangwon Park. 
+                        I'm really interested in the intersection of <b>HCI(Human-Computer Interaction)</b> and <b>AI(Artificial Intelligence)</b>. 
+                        I design and build systems that help make our lives healthier and more connected. 
+                    </p>
+                </div>
+                <div className='aboutme-figure'>
+
+                </div>
+            </div>
         </div>
     );
 }
