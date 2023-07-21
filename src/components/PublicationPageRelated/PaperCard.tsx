cbdfa5c002f9c68d251c49bfdf45d70cbb03aada
sangwonme <paulmoguri@naver.com>
2023-07-21 19:24:08 +0900

    :sparkles: Add iconFeature onclick function - open window w/ link && copy the bibTex

--- src/components/PublicationPageRelated/PaperCard.tsx
+++ src/components/PublicationPageRelated/PaperCard.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 
+import { useState } from 'react';
+
 import {FaQuoteLeft, FaAward, FaFilePdf, FaVideo, FaImage} from 'react-icons/fa';
+import { FaCircleCheck } from 'react-icons/fa6';
 
 import './PaperCard.scss';
 import PaperCardIcon from './PaperCardIcon';
@@ -42,6 +45,12 @@ const PaperCard = ({
   category
 }:PaperCardProps) => {
 
+  // show popup 
+  const [showPopup, setShowPopup] = useState<boolean>(false);
+  const setShowPopupProp = showPopup ? ()=>{} : setShowPopup;
+
+  console.log(showPopup);
+
   return (<>
     <div className='papercard-container'>
       <div className='papercard-thumbnail'>
@@ -49,11 +58,11 @@ const PaperCard = ({
       </div>
       <div className='papercard-info'>
         <div className='papercard-icons'>
-          <PaperCardIcon icon={itemToIcon['award']} item='award' data={award}/>
-          <PaperCardIcon icon={itemToIcon['pdf']} item='pdf' data={pdf}/>
-          <PaperCardIcon icon={itemToIcon['bibtex']} item='bibtex' data={bibtex}/>
-          <PaperCardIcon icon={itemToIcon['video']} item='video' data={video}/>
-          <PaperCardIcon icon={itemToIcon['slide']} item='slide' data={slide}/>
+          <PaperCardIcon setShowPopup={setShowPopupProp} icon={itemToIcon['award']} item='award' data={award}/>
+          <PaperCardIcon setShowPopup={setShowPopupProp} icon={itemToIcon['pdf']} item='pdf' data={pdf}/>
+          <PaperCardIcon setShowPopup={setShowPopupProp} icon={itemToIcon['bibtex']} item='bibtex' data={bibtex}/>
+          <PaperCardIcon setShowPopup={setShowPopupProp} icon={itemToIcon['video']} item='video' data={video}/>
+          <PaperCardIcon setShowPopup={setShowPopupProp} icon={itemToIcon['slide']} item='slide' data={slide}/>
         </div>
         <p className='papercard-title'>{title}</p>
         <p className='papercard-authors'>
@@ -64,6 +73,18 @@ const PaperCard = ({
         <p className='papercard-venue'>{venue}</p>
       </div>
     </div>
+    {
+      showPopup &&
+      <div className='absolute-screen'>
+        <div className='popup-container'>
+          <div className='popup-background'>
+            <FaCircleCheck className='fa-icon'/>
+            <p className='popup-text'>BibTeX copied to clipboard..!</p>
+          </div>
+        </div>
+      </div>
+
+    }
   </>);
 }
 
