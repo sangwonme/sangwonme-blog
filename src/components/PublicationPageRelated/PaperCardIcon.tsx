5628ca1609c3cb9b94d1cf68628dda6652aa6217
sangwonme <paulmoguri@naver.com>
2023-07-21 15:41:21 +0900

    :sparkles: Add PaperCardIcon styles

--- src/components/PublicationPageRelated/PaperCardIcon.tsx
+++ src/components/PublicationPageRelated/PaperCardIcon.tsx
@@ -2,11 +2,17 @@ import * as React from 'react';
 
 import { IconBaseProps } from 'react-icons';
 
+import './PaperCardIcon.scss'
+
 interface PaperCardIconProps {
     icon: React.ComponentType<IconBaseProps>;
     item: string;
     data: string;
-  }
+}
+
+function capitalizeFirstLetter(string:string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
   
 const PaperCardIcon: React.FC<PaperCardIconProps> = ({ icon: IconComponent, item, data }) => {
 return (
@@ -15,8 +21,8 @@ return (
         data !== "" && 
         (
         <div className='papercard-icon-wrapper'>
-            <div className='papercard-icon-item'><IconComponent /></div>
-            <p className='papercard-icon-desc'>{item==='award'? data : item}</p>
+            <div className='papercard-icon-icon'><IconComponent className='fa-icon'/></div>
+            <p className='papercard-icon-desc'>{item==='award'? capitalizeFirstLetter(data) : capitalizeFirstLetter(item)}</p>
         </div>
         )
     }
