cc979c097fa598995a4ec74ef352a29694afa9cd
sangwonme <paulmoguri@naver.com>
2025-04-21 17:07:44 +0900

    :sparkles: Add Header responsive

--- src/components/LayoutRelated/Header.tsx
+++ src/components/LayoutRelated/Header.tsx
@@ -10,8 +10,6 @@ const Header = () => {
                 <Link to='/'><p>Home</p></Link>
                 <div className='head-nav'>
                     <Link to='/publication'><p>Publication</p></Link>
-                    {/* <Link to='/building'><p>Project</p></Link> */}
-                    {/* <Link to='/artwork'><p>Artwork</p></Link> */}
                     <a href='https://www.instagram.com/_chunguri_/' target='_blank'><p>Artwork</p></a>
                 </div>
             </div>
